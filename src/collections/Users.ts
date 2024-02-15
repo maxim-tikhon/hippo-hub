52c5d4b7407ba2df671c8eaf88cee48461875c13
Maxim Tikhon <tikhon.maxim@gmail.com>
2024-02-15 03:59:16 +0100

    fix style issues, minor updates

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,61 +1,60 @@
-// import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
+import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
 import { Access, CollectionConfig } from 'payload/types'
 
-// const adminsAndUser: Access = ({ req: { user } }) => {
-//   if (user.role === 'admin') return true
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === 'admin') return true
 
-//   return {
-//     id: {
-//       equals: user.id,
-//     },
-//   }
-// }
+  return {
+    id: {
+      equals: user.id,
+    },
+  }
+}
 
 export const Users: CollectionConfig = {
   slug: 'users',
   auth: {
     verify: {
       generateEmailHTML: ({ token }) => {
-        return `<a href=${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}>Verify account</a>`
-        // return PrimaryActionEmailHtml({
-        //   actionLabel: "verify your account",
-        //   buttonText: "Verify Account",
-        //   href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
-        // })
+        return PrimaryActionEmailHtml({
+          actionLabel: "verify your account",
+          buttonText: "Verify Account",
+          href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+        })
       },
     },
   },
   access: {
-    read: () => true,
+    read: adminsAndUser,
     create: () => true,
-    // update: ({ req }) => req.user.role === 'admin',
-    // delete: ({ req }) => req.user.role === 'admin',
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
   },
-  // admin: {
-  //   hidden: ({ user }) => user.role !== 'admin',
-  //   defaultColumns: ['id'],
-  // },
+  admin: {
+    hidden: ({ user }) => user.role !== 'admin',
+    defaultColumns: ['id'],
+  },
   fields: [
-    // {
-    //   name: 'products',
-    //   label: 'Products',
-    //   admin: {
-    //     condition: () => false,
-    //   },
-    //   type: 'relationship',
-    //   relationTo: 'products',
-    //   hasMany: true,
-    // },
-    // {
-    //   name: 'product_files',
-    //   label: 'Product files',
-    //   admin: {
-    //     condition: () => false,
-    //   },
-    //   type: 'relationship',
-    //   relationTo: 'product_files',
-    //   hasMany: true,
-    // },
+    {
+      name: 'products',
+      label: 'Products',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'products',
+      hasMany: true,
+    },
+    {
+      name: 'product_files',
+      label: 'Product files',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'product_files',
+      hasMany: true,
+    },
     {
       name: 'role',
       defaultValue: 'user',
